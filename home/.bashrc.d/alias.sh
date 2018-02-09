813008456311baa71c947cece785d6ee339dc5ee
ha4gu <9391978+ha4gu@users.noreply.github.com>
2018-02-09 09:51:19 +0800

    mod aliases

--- home/.bashrc.d/alias.sh
+++ home/.bashrc.d/alias.sh
@@ -5,18 +5,12 @@ alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
 
-# Default to human readable figures
-alias df='df -h'
-alias du='du -h'
-
-# Misc :)
+# Misc
 alias less='less -r'                          # raw control characters
 alias whence='type -a'                        # where, of a sort
-alias grep='grep --color'                     # show differences in colour
-alias egrep='egrep --color=auto'              # show differences in colour
-alias fgrep='fgrep --color=auto'              # show differences in colour
-alias dir='ls --color=auto --format=vertical'
-alias vdir='ls --color=auto --format=long'
+alias grep='grep --color=always'              # show differences in colour
+alias egrep='egrep --color=always'            # show differences in colour
+alias fgrep='fgrep --color=always'            # show differences in colour
 alias ll='ls -la'                             # long list
 alias lll='ls -la'                            # 
 alias la='ls -A'                              # all but . and ..
@@ -26,12 +20,11 @@ alias j="jobs -l"
 alias gh="cd ~/"                              # gh = Go Home
 
 # Some shortcuts for different directory listings
-case "${OSTYPE}" in
-    darwin*)
-        alias ls='ls -hFG'
-        ;;
-    *)
-        alias ls='ls --human-readable --file-type --color=auto'
-        ;;
-esac
+if [[ ${OSTYPE} = darwin* ]]; then
+	alias ls='ls -hFG'
+elif [[ ${MSYSTEM} = MSYS ]]; then
+	alias ls='ls --human-readable --file-type --color=always --ignore="NTUSER.DAT{*}.TM*"'
+else
+	alias ls='ls --human-readable --file-type --color=always'
+fi
 
