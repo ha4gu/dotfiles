763be010183137b9a202c64ef44d5d243c535b4f
ha4gu <9391978+ha4gu@users.noreply.github.com>
2018-12-04 16:42:38 +0900

    mod .bashrc.d/rbenv.sh for locally installed rbenv command

--- home/.bashrc.d/rbenv.sh
+++ home/.bashrc.d/rbenv.sh
@@ -1,4 +1,8 @@
-# rbenv
+# looking for a locally installed rbenv
+[ -r "$HOME/.rbenv" ] \
+	&& PATH=$PATH:$HOME/.rbenv/bin
+
+# rbenv init
 if type rbenv > /dev/null 2>&1; then
   eval "$(rbenv init -)"
 fi
