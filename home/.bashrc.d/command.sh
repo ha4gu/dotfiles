3a4bbdf0799b4c7d0fde6bc7b75593a26081f8c5
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-02-04 19:19:22 +0900

    update for bash-completion@2

--- home/.bashrc.d/command.sh
+++ home/.bashrc.d/command.sh
@@ -1,13 +1,9 @@
-
-# make less more friendly for non-text input files, see lesspipe(1)
-[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
-
-# enable programmable completion features (you don't need to enable
-# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
-# sources /etc/bash.bashrc).
+# bash completion
 if type brew > /dev/null 2>&1; then
-  if [ -f $(brew --prefix)/etc/bash_completion ]; then
-    . $(brew --prefix)/etc/bash_completion
+  # for homebrew on macOS
+  if [ -r $(brew --prefix)/etc/profile.d/bash_completion.sh ]; then
+    export BASH_COMPLETION_COMPAT_DIR=$(brew --prefix)/etc/bash_completion.d
+    . $(brew --prefix)/etc/profile.d/bash_completion.sh
   fi
 elif [ -r /usr/share/bash-completion/bash_completion ]; then
   . /usr/share/bash-completion/bash_completion
@@ -15,21 +11,20 @@ elif [ -r /etc/bash_completion ]; then
   . /etc/bash_completion
 fi
 
-# ssh-pageant
-[ -r /usr/bin/ssh-pageant ] \
-	&& eval $(/usr/bin/ssh-pageant -r -a "/tmp/.ssh-pageant-$USERNAME")
-
 # homeshick
 [ -r "$HOME/.homesick/repos/homeshick/homeshick.sh" ] \
-	&& source "$HOME/.homesick/repos/homeshick/homeshick.sh" \
-	&& source "$HOME/.homesick/repos/homeshick/completions/homeshick-completion.bash"
+  && source "$HOME/.homesick/repos/homeshick/homeshick.sh" \
+  && source "$HOME/.homesick/repos/homeshick/completions/homeshick-completion.bash"
 
 # enhancd
 [ -r "$HOME/.enhancd/init.sh" ] \
-    && source "$HOME/.enhancd/init.sh"
+  && source "$HOME/.enhancd/init.sh"
 
-# Android ADB Command
-[ -r "$HOME/Library/Android/sdk/platform-tools" ] \
-	&& PATH=$PATH:$HOME/Library/Android/sdk/platform-tools
+# ssh-pageant on Windows
+[ -r /usr/bin/ssh-pageant ] \
+  && eval $(/usr/bin/ssh-pageant -r -a "/tmp/.ssh-pageant-$USERNAME")
+
+# lesspipe
+#[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
 
 return 0
