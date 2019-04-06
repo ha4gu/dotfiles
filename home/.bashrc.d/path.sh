3b02885f8ff1a7428e1f86bcc182358d9062dfd5
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-04-06 16:07:28 +0900

    set some paths

---
+++ home/.bashrc.d/path.sh
@@ -0,0 +1,12 @@
+# add ~/.linuxbrew for LinuxBrew
+export PATH="$HOME/.linuxbrew/bin:$PATH"
+export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
+export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
+export LD_LIBRARY_PATH="$HOME/.linuxbrew/lib:$LD_LIBRARY_PATH"
+
+# add ~/.local for my locally installed programs
+export PATH="$HOME/.local/bin:$PATH"
+export MANPATH="$HOME/.local/share/man:$MANPATH"
+export INFOPATH="$HOME/.local/share/info:$INFOPATH"
+export LD_LIBRARY_PATH="$HOME/.local/lib:$LD_LIBRARY_PATH"
+
