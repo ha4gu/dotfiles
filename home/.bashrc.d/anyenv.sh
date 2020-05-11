e323e0993a8ae2db9422a72056ef7e41bcc31467
ha4gu <9391978+ha4gu@users.noreply.github.com>
2020-05-11 19:59:57 +0900

    Quit using goenv

--- home/.bashrc.d/anyenv.sh
+++ home/.bashrc.d/anyenv.sh
@@ -13,12 +13,3 @@ if type pyenv > /dev/null 2>&1; then
   eval "$(pyenv init -)"
 fi
 
-# goenv for Go
-if [ -r "$HOME/.goenv/bin/goenv" ]; then
-  export PATH="$HOME/.goenv/bin:$PATH"
-fi
-
-if type goenv > /dev/null 2>&1; then
-  eval "$(goenv init -)"
-fi
-
