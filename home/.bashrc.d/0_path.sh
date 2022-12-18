bd8e1b29d9a31d8eb0b61e738d4c558533e5f77b
Yuta Hashiguchi <9391978+ha4gu@users.noreply.github.com>
2022-12-18 16:07:20 +0900

    利用していないLinuxbrew関連パスを削除

--- home/.bashrc.d/0_path.sh
+++ home/.bashrc.d/0_path.sh
@@ -1,12 +1,6 @@
 # add /usr/local/sbin for Homebrew
 export PATH="/usr/local/sbin:$PATH"
 
-# add ~/.linuxbrew for Linuxbrew
-export PATH="$HOME/.linuxbrew/bin:$PATH"
-export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
-export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
-export LD_LIBRARY_PATH="$HOME/.linuxbrew/lib:$LD_LIBRARY_PATH"
-
 # add ~/.local for my locally installed programs
 export PATH="$HOME/.local/bin:$PATH"
 export MANPATH="$HOME/.local/share/man:$MANPATH"
@@ -17,4 +11,3 @@ export LD_LIBRARY_PATH="$HOME/.local/lib:$LD_LIBRARY_PATH"
 if [ -r '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc' ]; then
   source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc'
 fi
-
