7a4940cd4a1f4bbbf299d42bf486d9c4c9c5fcff
Yuta Hashiguchi <9391978+ha4gu@users.noreply.github.com>
2025-10-04 11:51:21 +0900

    rdk-dlp関数を追加

--- home/.bashrc.d/command.sh
+++ home/.bashrc.d/command.sh
@@ -18,3 +18,18 @@ if [ $(uname -s) = "Darwin" ] \
   source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc"
   source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc"
 fi
+
+# radiko
+function rdk-dlp() {
+  # 引数がない場合には即終了
+  if [ $# -eq 0 ]; then
+    echo "URLを指定してください"
+    return 1
+  fi
+
+  # 各URLに対してループ処理
+  for url in "$@"; do
+    echo "Processing URL: $url"
+    yt-dlp -N 30 --embed-metadata --embed-thumbnail -o "%(title)s %(timestamp+32400>%Y-%m-%d_%H%M)s [%(id)s].%(ext)s" "$url"
+  done
+}
